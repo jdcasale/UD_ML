a358a461df60e9fa5e463b81aab837c9cb950ff7
Josh Casale <josh.casale@sproutsocial.com>
2016-02-06 22:53:10 -0600

    Revision 2

--- boston_housing_prices/housing_prices.py
+++ boston_housing_prices/housing_prices.py
@@ -67,7 +67,7 @@ def split_data(city_data):
     ###################################
 
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(
-    X, y, test_size=0.4, random_state=0)
+    X, y, test_size=0.3, random_state=0)
 
     #return X_train, y_train, X_test, y_test
 
